d3f04352e3a0860624c039a1125d74a610b60bf6
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-02-02 20:16:55 -0600

    feat: add claim conditions to our only one token in the bundle drop

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,20 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule('0x989BAd4F17E96d38a1B073CcE2461f9722f2282c');
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})();
